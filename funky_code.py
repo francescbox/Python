978b17a6f92a10f2138d227f2513760fcfc223d4
MisterBox <boxfrancesc@gmail.com>
2023-10-22 15:12:04 +0100

    Second upload

---
+++ funky_code.py
@@ -0,0 +1,14 @@
+char = '#'
+
+print (f" {char*4}")
+print (f"{char} ")
+print (f"{char} ")
+print (f"{char} ")
+print (f" {char*4}")
+
+print ("\n")
+
+
+print (f"{char*4}")
+print (f"{char + '    ' + char}")
+print (f"{char*4}")
